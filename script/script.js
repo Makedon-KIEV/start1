c70b0fad5f18e4b042d01759f9577d1317da997d
Alexandr <43139323+Makedon-KIEV@users.noreply.github.com>
2022-01-16 14:48:35 +0200

    commit in js
    je false

--- script/script.js
+++ script/script.js
@@ -1,8 +1,21 @@
 let addMessage = document.querySelector('.message'),
-addButton = document.querySelector('.add')
+  addButton = document.querySelector('.add'),
+  todo = document.querySelector('.todo');
+// let removeButton = document.querySelector('.remove');
+// let doneButton = document.querySelector('.done');
+// let clearButton = document.querySelector('.clear');
+
+let todoList = [];
 
 addButton.addEventListener('clik', function () {
 
-  
-  console.log(addMessage.value);
-})
+  let newTodo = {
+    todo: addMessage.value,
+    checked: false,
+    important: false
+  }
+
+  todoList.push(newTodo);
+  console.log(todoList);
+
+});
