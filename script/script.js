1ea5232589c5ac13a68b976d5bb2b9da7a1a8873
Alexandr <43139323+Makedon-KIEV@users.noreply.github.com>
2022-01-16 14:27:58 +0200

    commit list
    js commit new button

---
+++ script/script.js
@@ -0,0 +1,8 @@
+let addMessage = document.querySelector('.message'),
+addButton = document.querySelector('.add')
+
+addButton.addEventListener('clik', function () {
+
+  
+  console.log(addMessage.value);
+})
